a2b94159786e80260b003c782438b82446903644
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-02-23 12:34:53 +0000

    Cahpter 5

---
+++ Chapter5/favorite_number.rb
@@ -0,0 +1,4 @@
+puts "I always think favorites numbers tell something about people, what's yours?"
+number = gets.chomp.to_i
+better = number + 1
+puts "Oooooh!! So close... #{better} is the best number possible to me. But yours is pretty good too!"
---
+++ Chapter5/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts "Well, hello there, chaps. May I ask for your first name?"
+first = gets.chomp
+puts "And what about your surname ?"
+surname = gets.chomp
+puts "Hmmmm... #{first} #{surname} sounds really nice! And what's your middle name?"
+middle = gets.chomp
+puts "Ok! #{first} #{middle} #{surname}, you are now part of the team!"
