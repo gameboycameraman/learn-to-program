64b84b2e104d3bf663bdc84a111fb10983396ca2
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-02-26 20:19:52 +0100

    Old Roman Numeral

--- Chapter9/old_roman_numeral.rb
+++ Chapter9/old_roman_numeral.rb
@@ -1,9 +1,30 @@
-puts "You always wanted to know what your favorite number would look like in roman numeral?"
-puts "Now you can, type here a number and press ENTER!"
-num = gets.chomp
+def old_roman_numeral(num)
+  num / 1000 > 0
+  mille = num / 1000
+  roman_mille = "M" * mille
 
-def old_roman_numeral
-  
+  cent = num % 1000
+  cent_signe = cent / 100
+  roman_cent = "C" * cent_signe
+  if roman_cent.count("C") >= 5
+    roman_cent.gsub!("CCCCC", "D")
+  end
+
+  dizaine = num % 100
+  dizaine_signe = dizaine / 10
+  roman_dix = "X" * dizaine_signe
+  if roman_dix.count("X") >= 5
+    roman_dix.gsub!("XXXXX", "L")
+  end
+
+  unite_signe = num % 10
+  roman_unite = "I" * unite_signe
+  if roman_unite.count("I") >= 5
+    roman_unite.gsub!("IIIII", "V")
+  end
+
+return roman_mille + roman_cent + roman_dix + roman_unite
+
 end
 
-puts m
+puts old_roman_numeral(2778)
